fc0a95ed184e57cb80a4861243bdca1bd459c94c
Sokyran <toCy&#$VIH$1cSOu8s&Fv@UVN%L3snN*H09$1^Xghq*hKo9Itnmq#bNf8Ld67#T$#a&cEaY1>
2022-11-15 17:41:18 +0100

    Added a transpiler to increase max packet ID supported by MP

---
+++ Source/Cosmodrome/Core/Multiplayer/IncreaseMaxAllowedPacketId.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
+using Multiplayer.Common;
+using Verse;
+
+namespace RocketMan;
+
+[RocketPatch(typeof(ConnectionBase), nameof(ConnectionBase.HandleReceive), parameters = new []{ typeof(int), typeof(int), typeof(ByteReader), typeof(bool) })]
+public static class IncreaseMaxAllowedPacketId
+{
+    public static bool finishedSuccessfully;
+
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instr)
+    {
+        var finished = false;
+        
+        foreach (var ci in instr)
+        {
+            yield return ci;
+
+            if (!finished && ci.opcode == OpCodes.Ldc_I4_S && ci.operand is sbyte val)
+            {
+                finished = true;
+                ci.operand = val + 1;
+            }
+        }
+
+        finishedSuccessfully = finished;
+        if (!finished)
+            Log.Error($"ROCKETMAN: Failed patching max multiplayer packet ID, camera-related features will be disabled (time dilation, corpse cleanup)");
+    }
+}
